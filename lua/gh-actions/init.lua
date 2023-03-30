ddf9978f250f8018df5392bb37c2d6c151692d09
Damian Senn <damian.senn@topaxi.codes>
2023-03-30 14:51:46 +0200

    bleh

--- lua/gh-actions/init.lua
+++ lua/gh-actions/init.lua
@@ -2,32 +2,32 @@ local event = require("nui.utils.autocmd").event
 local Split = require("nui.split")
 
 local split = Split({
-	position = "right",
-	size = 40,
-	border = {
-		text = {
-			top = "Github Workflows",
-		},
-	},
+  position = "right",
+  size = 40,
+  border = {
+    text = {
+      top = "Github Workflows",
+    },
+  },
 })
 
 local M = {
-	setup_called = false,
-	init_root = "",
+  setup_called = false,
+  init_root = "",
 }
 
 function M.setup()
-	M.init_root = vim.fn.getcwd()
+  M.init_root = vim.fn.getcwd()
 
-	M.setup_called = true
+  M.setup_called = true
 end
 
 function M.open()
-	split:mount()
+  split:mount()
 end
 
 function M.close()
-	split:unmount()
+  split:unmount()
 end
 
 return M
