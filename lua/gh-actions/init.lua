2db25f340cc83f115f7acaf66ee9282ce5f4a27e
Damian Senn <damian.senn@topaxi.codes>
2023-03-30 14:50:23 +0200

    initial commit

---
+++ lua/gh-actions/init.lua
@@ -0,0 +1,33 @@
+local event = require("nui.utils.autocmd").event
+local Split = require("nui.split")
+
+local split = Split({
+	position = "right",
+	size = 40,
+	border = {
+		text = {
+			top = "Github Workflows",
+		},
+	},
+})
+
+local M = {
+	setup_called = false,
+	init_root = "",
+}
+
+function M.setup()
+	M.init_root = vim.fn.getcwd()
+
+	M.setup_called = true
+end
+
+function M.open()
+	split:mount()
+end
+
+function M.close()
+	split:unmount()
+end
+
+return M
