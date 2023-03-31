814d17594767e6079d8f8d3ff1c944f60202718b
Damian Senn <damian.senn@scout24.ch>
2023-03-31 08:18:44 +0200

    extract gh related functions into its own module

---
+++ lua/gh-actions/github.lua
@@ -0,0 +1,69 @@
+local curl = require("plenary.curl")
+local job = require("plenary.job")
+
+local M = {}
+
+---@class GhWorkflow
+---@field id number
+---@field node_id string
+---@field name string
+---@field path string
+---@field state string
+---@field created_at string
+---@field updated_at string
+---@field url string
+---@field html_url string
+---@field badge_url string
+
+---@class GhWorkflowsResponse
+---@field total_count number
+---@field workflows GhWorkflow[]
+
+---@return string
+local function read_gh_hosts_token()
+  -- TODO: Can we not depend on `yq` for parsing yml?
+  local yq = job:new({
+    command = "yq",
+    args = { "-o", "json", vim.fn.expand("$HOME/.config/gh/hosts.yml") },
+  })
+
+  yq:sync()
+
+  local jsonStr = table.concat(yq:result(), "\n")
+  local ghHostsConfig = vim.json.decode(jsonStr)
+
+  return ghHostsConfig["github.com"].oauth_token
+end
+
+---@return string
+local function get_github_token()
+  return vim.env.GITHUB_TOKEN
+    or read_gh_hosts_token()
+    or assert(nil, "No GITHUB_TOKEN found in env and no gh cli config found")
+end
+
+---@param path string
+function M.fetch(path)
+  return curl.get(string.format("https://api.github.com%s", path), {
+    headers = {
+      Authorization = string.format("Bearer %s", get_github_token()),
+    },
+  })
+end
+
+---@param repo string
+---@return GhWorkflow[]
+function M.get_workflows(repo)
+  local response = M.fetch(string.format("/repos/%s/actions/workflows", repo))
+
+  if not response then
+    return {}
+  end
+
+  ---@type GhWorkflowsResponse | nil
+  local responseData = vim.json.decode(response.body)
+
+  return responseData and responseData.workflows or {}
+end
+
+return M
--- lua/gh-actions/init.lua
+++ lua/gh-actions/init.lua
@@ -1,69 +1,5 @@
 local Split = require("nui.split")
-local curl = require("plenary.curl")
-local job = require("plenary.job")
-
----@class GhWorkflow
----@field id number
----@field node_id string
----@field name string
----@field path string
----@field state string
----@field created_at string
----@field updated_at string
----@field url string
----@field html_url string
----@field badge_url string
-
----@class GhWorkflowsResponse
----@field total_count number
----@field workflows GhWorkflow[]
-
----@return string
-local function read_gh_hosts_token()
-  -- TODO: Can we not depend on `yq` for parsing yml?
-  local yq = job:new({
-    command = "yq",
-    args = { "-o", "json", vim.fn.expand("$HOME/.config/gh/hosts.yml") },
-  })
-
-  yq:sync()
-
-  local jsonStr = table.concat(yq:result(), "\n")
-  local ghHostsConfig = vim.json.decode(jsonStr)
-
-  return ghHostsConfig["github.com"].oauth_token
-end
-
----@return string
-local function get_github_token()
-  return vim.env.GITHUB_TOKEN
-    or read_gh_hosts_token()
-    or assert(nil, "No GITHUB_TOKEN found in env and no gh cli config found")
-end
-
----@param path string
-local function fetch_github(path)
-  return curl.get(string.format("https://api.github.com%s", path), {
-    headers = {
-      Authorization = string.format("Bearer %s", get_github_token()),
-    },
-  })
-end
-
----@param repo string
----@return GhWorkflow[]
-local function get_github_workflows(repo)
-  local response = fetch_github(string.format("/repos/%s/actions/workflows", repo))
-
-  if not response then
-    return {}
-  end
-
-  ---@type GhWorkflowsResponse | nil
-  local responseData = vim.json.decode(response.body)
-
-  return responseData and responseData.workflows or {}
-end
+local gh = require("gh-actions.github")
 
 local split = Split({
   position = "right",
@@ -84,7 +20,7 @@ end
 function M.open()
   split:mount()
 
-  local workflows = get_github_workflows("topaxi/learning-cs")
+  local workflows = gh.get_workflows("topaxi/learning-cs")
 
   print(vim.inspect(workflows))
 end
