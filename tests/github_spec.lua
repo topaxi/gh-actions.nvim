dcca220830337c2335064f2070d30ddd84ab195f
Damian Senn <damian.senn@scout24.ch>
2023-04-04 13:08:33 +0200

    test: reset get_github_token env

--- tests/github_spec.lua
+++ tests/github_spec.lua
@@ -1,9 +1,21 @@
 local gh = require("gh-actions.github")
 
 describe("get_github_token", function()
-  it("it should read from gh/hosts.yml", function()
+  before_each(function()
+    vim.env.GITHUB_TOKEN = nil
+  end)
+
+  it("should read from gh/hosts.yml", function()
     local token = gh.get_github_token("tests/fixtures/gh_hosts.yml")
 
     assert.are.same("topaxitesttoken", token)
+  end)
+
+  it("should read from GITHUB_TOKEN", function()
+    vim.env.GITHUB_TOKEN = "envtoken"
+
+    local token = gh.get_github_token("tests/fixtures/gh_hosts.yml")
+
+    assert.are.same("envtoken", token)
   end)
 end)
