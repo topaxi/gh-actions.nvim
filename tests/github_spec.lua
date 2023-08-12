a466e533493975baaf078724a00503f3ff07df56
Damian Senn <damian.senn@topaxi.codes>
2023-08-12 15:28:29 +0200

    fix: test case

--- tests/github_spec.lua
+++ tests/github_spec.lua
@@ -1,21 +1,21 @@
-local gh = require("gh-actions.github")
+local gh = require('gh-actions.github')
 
-describe("get_github_token", function()
+describe('get_github_token', function()
   before_each(function()
     vim.env.GITHUB_TOKEN = nil
   end)
 
-  it("should read from gh/hosts.yml", function()
-    local token = gh.get_github_token("echo etopaxitesttoken")
+  it('should read from gh/hosts.yml', function()
+    local token = gh.get_github_token('echo topaxitesttoken')
 
-    assert.are.same("topaxitesttoken", token)
+    assert.are.same('topaxitesttoken', token)
   end)
 
-  it("should read from GITHUB_TOKEN", function()
-    vim.env.GITHUB_TOKEN = "envtoken"
+  it('should read from GITHUB_TOKEN', function()
+    vim.env.GITHUB_TOKEN = 'envtoken'
 
-    local token = gh.get_github_token("echo etopaxitesttoken")
+    local token = gh.get_github_token('echo topaxitesttoken')
 
-    assert.are.same("envtoken", token)
+    assert.are.same('envtoken', token)
   end)
 end)
